f9de8a95ae4bd947a24eb747ab440ad003cf1835
DevMania <devmania1223@gmail.com>
2022-11-23 01:46:38 +0800

    upload

---
+++ src/Main.tsx
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import App from "./App";
+import Login from "./Login";
+import { UserContext } from "./utils/contexts/UserContext";
+
+const Main: React.FC = () => {
+  const { state } = useContext(UserContext);
+  // return !!state.user ? <App /> : <Login />;
+  return <App/>;
+};
+
+export default Main;
