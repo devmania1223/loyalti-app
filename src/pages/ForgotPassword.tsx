f2306dd174fb966253829cdc91cafc677643533c
Satharuban S <satharuban97@gamil.com>
2022-12-13 00:02:56 +0530

    Logo issue fixed

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -2,6 +2,7 @@ import { App } from "@capacitor/app";
 import { IonContent, IonPage } from "@ionic/react";
 import React, { useContext, useEffect } from "react";
 import { TasksContext } from "../utils/contexts/TasksContext";
+import Logo from "../images/logo.png"
 
 const ForgotPassword: React.FC = () => {
   const { state, dispatch } = useContext(TasksContext);
@@ -19,7 +20,7 @@ const ForgotPassword: React.FC = () => {
           <div className="mt-5 h-[50px]"></div>
           <div className="py-[5] mt-10 mx-10">
             <img
-              src="https://s3-alpha-sig.figma.com/img/703c/8891/5533c5f17c442957a1620e49d4b8c0db?Expires=1670198400&Signature=LG7Cis6JiSAoCyTZJAsp6Lxt1cwh32FpHHtqgUDUxUIcOOmuVJ-8WhdYhlMi8Fwrp7mpXtq2sXXs832CE4dlRnPrnwCMlppKzoAOxugnCNOh6IPEckEfccvHDU2qJ~iDcM940nrFXM~iejTl50CmWKszbm7tjl4nMc6XKOdwsCis5xeAIgWPb4GsUfHxBoj8MmImW2tQnxzT5CSYB0wLUA--icZlogKbYyxfrphDXFAcwTaXqmUZwkQUMp19Vd3ybY2Q1DpWyqHghOx2SNt787hGSdvbflaIWk6NyT7ngyP7hboGNOYBmf95mWywUuvxnY-X8bgbh-trQKgp6BxF~Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+              src={Logo}
               alt=" "
               className="w-[100%] px-[5]"
             />
